cbf97043e29fb0fa0efb874a1c7027b9a2eb2e89
thomas descoteaux <thomas.descoteaux@umontreal.ca>
2024-09-23 15:30:01 -0400

    end merging

---
+++ src/nrgten/model_ensemble.py
@@ -0,0 +1,91 @@
+from modeller import Environ, Model, Alignment
+from modeller.automodel import *
+from modeller.selection import selection
+from pymol import cmd
+from src.surfaces.clean_structure import main as clean_structure
+from PyQt5.QtWidgets import QApplication
+import os
+
+def model_states(input_file, target, temp_path, main_folder_path, form):
+    model_data = []
+    current_model = []
+    model_dir = os.path.join(temp_path,'NRGTEN', f'{target}_models')
+    os.makedirs(model_dir,exist_ok=True)
+
+    with open(input_file, 'r') as file:
+        for line in file:
+            if line.startswith('MODEL'):
+                if current_model:
+                    model_data.append(current_model)
+                current_model = [line]
+            elif line.startswith('ENDMDL'):
+                current_model.append(line)
+                model_data.append(current_model)
+                current_model = []
+            else:
+                if current_model:
+                    current_model.append(line)
+    if current_model:
+        model_data.append(current_model)
+
+    # Set the maximum value for the progress bar
+    form.NRGten_progress.setMaximum(len(model_data))
+
+    # Iterate over each model and process it
+    for i, model in enumerate(model_data):
+        model_output_path = os.path.join(model_dir, f'model_{i + 1}.pdb')
+        with open(model_output_path, 'w') as output_file:
+            output_file.writelines(model)
+
+        code = os.path.join(model_dir, f'model_{i + 1}')
+        cleaned_file_path = code + '_clean.pdb'
+
+        # Clean PDB file
+        clean_pdb_file = open(cleaned_file_path, "w")
+        clean_structure(code + '.pdb', os.path.join(main_folder_path, "deps", "surfaces", 'AMINO_FlexAID.def'),
+                        clean_pdb_file)
+        clean_pdb_file.close()
+
+        # Run model state alignment and generation
+        model_state(cleaned_file_path, model_dir)
+
+        # Update UI elements: progress bar and label
+        form.NRGten_progress.setValue(i + 1)
+        form.NRGten_label.setText(f'State: {i + 1}/{len(model_data)}')
+
+        # Process events to keep the UI responsive
+        QApplication.processEvents()
+
+        # Load model into PyMOL and show as cartoon
+        cmd.load(cleaned_file_path, f'{target}_ensemble', state=i + 1)
+        cmd.show('cartoon', f'{target}_ensemble')
+        if i==5:
+            break
+
+
+
+def model_state(cleaned_file_path, model_dir):
+    pdb_path = cleaned_file_path
+    env = Environ()
+    mdl = Model(env, file=pdb_path)
+    aln = Alignment(env)
+    aln.append_model(mdl, align_codes='pdb_target', atom_files=pdb_path)
+    mdl = Model(env, file=pdb_path)
+    aln.append_model(mdl, align_codes='pdb_template', atom_files=pdb_path)
+    aln.align2d()
+    aln.write(file='alignment.ali', alignment_format='PIR')
+
+    class MyModel(automodel):
+        def select_atoms(self):
+            return selection(self)
+
+    output_dir = os.path.join(model_dir, 'output_models')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    a = MyModel(env, alnfile='alignment.ali', knowns='pdb_template', sequence='pdb_target')
+    a.starting_model = 1
+    a.ending_model = 1
+    a.very_fast()
+    a.make()
+    os.rename('pdb_target.B99990001.pdb', cleaned_file_path)
---
+++ src/surfaces/surface_cont.py
@@ -0,0 +1,210 @@
+# Developed by Natália Teruel
+# Najmanovich Research Group
+# Cite this work as Surfaces: A software to quantify and visualize interactions within and between proteins and ligands - Teruel, N. F. B., Borges, V. M., & Najmanovich, R. (2023)
+
+#Imports
+import argparse
+import sys
+import os
+import re
+import pandas as pd
+
+
+#Useful dicts
+aa = {'C':'CYS', 'D':'ASP', 'S':'SER', 'Q':'GLN', 'K':'LYS', 'I':'ILE', 'P':'PRO', 'T':'THR', 'F':'PHE', 'N':'ASN', 'G':'GLY', 'H':'HIS', 'L':'LEU', 'R':'ARG', 'W':'TRP', 'A':'ALA', 'V':'VAL', 'E':'GLU', 'Y':'TYR', 'M':'MET'}
+
+#Input of pdb file and the 2 chains between which we want to evaluate the interactions
+
+def read_residues(pdb_file, chain1, chain2):
+    list_chain1 = []
+    list_chain2 = []
+    f = open(pdb_file, 'r')
+    Lines = f.readlines()
+    for line in Lines:
+        if (line[:4] == 'ATOM' or line[:4] == 'HETA'):
+            if line[21] in chain1:
+                res_num = re.findall('[+-]?\d+',line[22:27])
+                res_name = line[17:20]
+                string = res_name + str(res_num[0]) + line[21]
+                if string not in list_chain1:
+                    list_chain1.append(string)
+            if line[21] in chain2:
+                res_num = re.findall('[+-]?\d+',line[22:27])
+                res_name = line[17:20]
+                string = res_name + str(res_num[0]) + line[21]
+                if string not in list_chain2:
+                    list_chain2.append(string)
+    chains = chain1 + chain2
+    atoms_numbers = []
+    for k in range(len(chains)):
+        atoms_numbers.append([])
+    for line in Lines:
+        for i in range(len(chains)):
+            if (line[:4] == 'ATOM' or line[:4] == 'HETA') and line[21] == chains[i]:
+                atoms_numbers[i].append(int(line[6:12]))
+    return (list_chain1, list_chain2, chains, atoms_numbers)
+
+#Function to generate the file with the output of perl code vcont.pl
+
+def vcon(pdb_name,vcon_binary_path):
+    string = f'{vcon_binary_path} {pdb_name} > vcon_file.txt'
+    os.system(string)
+
+#Functions to fix the names of the chains
+
+def get_chain(atom, og_chain, chains, atom_numbers):
+    if og_chain in chains:
+        index = chains.index(og_chain)
+    else:
+        return (og_chain)
+    if atom in atom_numbers[index]:
+        return (og_chain)
+    else:
+        for i in range(len(chains)):
+            if i != index:
+                if atom in atom_numbers[i]:
+                    return (chains[i])
+    return (0)
+
+#Functions to open vcon results, .def and .dat for atom types interactions
+
+def read_atom(line):
+    atnum = int(line[:6])
+    attype = line[8:11]
+    resnum = int(line[12:17])
+    res = line[19:22]
+    chain = line[23:24]
+    return (atnum,attype,resnum,res,chain)
+
+def read_surface(line):
+    surf = (float(line[-12:-6]))
+    return (surf)
+
+def read_interactions(file, matrix, chain1, chain2, def_file, dat_file, atom_numbers, scale_factor):
+    chains = chain1 + chain2
+    f = open(file, 'r')
+    Lines = f.readlines()
+    for line in Lines:
+        if line[:1] != '#' and line != '\n':
+            if line[31:34] == 'Sol':
+                main_line = line
+                main_atnum,main_attype,main_resnum,main_res,main_chain = read_atom(main_line)
+                fixed_main_chain = get_chain(main_atnum,main_chain,chains,atom_numbers)
+                if fixed_main_chain != 0:
+                    main_residue = main_res+str(main_resnum)+fixed_main_chain
+            else:
+                atnum,attype,resnum,res,chain = read_atom(line[22:])
+                fixed_other_chain = get_chain(atnum,chain,chains,atom_numbers)
+                if fixed_other_chain != 0:
+                    other_residue = res+str(resnum)+fixed_other_chain
+                    surf = read_surface(line)
+                    if (fixed_other_chain in chain1 and fixed_main_chain in chain2) or (fixed_other_chain in chain2 and fixed_main_chain in chain1):
+                        if fixed_main_chain in chain2:
+                            matrix.loc[other_residue, main_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file) * scale_factor)/2
+                        if fixed_main_chain in chain1:
+                            matrix.loc[main_residue, other_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file) * scale_factor)/2
+  
+    return(matrix)
+
+#get the atom type number from def file of choice
+def atomtype_num(def_file, res, attyp):
+    attyp = attyp.replace(" ", "")
+    f = open(def_file, 'r')
+    Lines = f.readlines()
+    for i in range(len(Lines)):
+        if Lines[i][:3] == res:
+            ind = Lines[i].index(attyp+':')
+            ind_end = Lines[i][ind:].index(',')
+            attype_num = int(Lines[i][ind+len(attyp)+1:ind+ind_end])
+    f.close()
+    return(attype_num)
+
+#get the interaction between atom type 1 and atom type 2 from dat file of choice
+def interactions(dat_file, at1, at2):
+    if len(str(at1)) == 1:
+        at1 = ' ' + str(at1)
+    if len(str(at2)) == 1:
+        at2 = ' ' + str(at2)
+    f = open(dat_file, 'r')
+    Lines = f.readlines()
+    for line in Lines:
+        if str(at1)+'-'+str(at2) == line[5:10] or str(at2)+'-'+str(at1) == line[5:10]:
+            interact = float(line[13:])
+    f.close()
+    return(interact)
+
+#get the final score based on atom type num and interactions
+def score(attype1, res1, attype2, res2, def_file, dat_file):
+    at1 = atomtype_num(def_file, res1, attype1)
+    at2 = atomtype_num(def_file, res2, attype2)
+    value = interactions(dat_file, at1, at2)
+    return (value)
+
+#create file of list of interactions
+def list_file(matrix,output_name):
+    residues1 = []
+    residues2 = []
+    values = []
+    abs_values = []
+    for i in range(len(matrix.index)):
+        for j in range(len(matrix.columns)):
+            num = matrix.loc[matrix.index[i], matrix.columns[j]]
+            if num != 0:
+                residues1.append(matrix.index[i])
+                residues2.append(matrix.columns[j])
+                values.append(num)
+                abs_values.append(abs(num))
+    sorted_residues1 = [x for _,x in sorted(zip(abs_values,residues1),reverse=True)]
+    sorted_residues2 = [x for _,x in sorted(zip(abs_values,residues2),reverse=True)]
+    sorted_values = [x for _,x in sorted(zip(abs_values,values),reverse=True)]
+    f = open(os.path.join(os.path.dirname(output_name),"List_" + os.path.basename(output_name[:-4]) + ".txt"), "w")
+    for k in range(len(values)):
+        f.write(sorted_residues1[k] + "," + sorted_residues2[k] + "," + str(sorted_values[k]) + "\n")
+    f.close()
+    return
+
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser(description="the arguments.", add_help=False)
+    parser.add_argument("-f", "--pdb_file", action="store")
+    parser.add_argument("-c1", "--chain1", action="store")
+    parser.add_argument("-c2", "--chain2", action="store")
+    parser.add_argument("-o", "--output_name", action="store")
+    parser.add_argument("-def", "--atomtypes_definition", action="store")
+    parser.add_argument("-dat", "--atomtypes_interactions", action="store")
+    args = parser.parse_args()
+    pdb_file=args.pdb_file
+    chain1=args.chain1
+    chain2=args.chain2
+    output_name=args.output_name
+    atomtypes_definition=args.atomtypes_definition
+    atomtypes_interactions=args.atomtypes_interactions
+    main(pdb_file, chain1, chain2, output_name, atomtypes_definition, atomtypes_interactions,'.vcon')
+
+def main(pdb_file,chain1,chain2,output_name,atomtypes_definition,atomtypes_interactions,vcon_binary_path):
+
+    res1, res2, chains, atom_numbers = read_residues(pdb_file, chain1, chain2)
+    #print (res1, res2)
+    #print (chains, atom_numbers)
+        
+    vcon(pdb_file,vcon_binary_path)
+  
+    matrix = [ [ 0 for i in range(len(res2)) ] for j in range(len(res1)) ]
+    matrix = pd.DataFrame(matrix)
+    matrix.columns = res2
+    matrix.index = res1
+    
+    # Determined according to the AB-Bind dataset results
+    scale_factor = 0.00024329
+    
+    matrix = read_interactions('vcon_file.txt', matrix, chain1, chain2, atomtypes_definition, atomtypes_interactions, atom_numbers, scale_factor)
+        
+    matrix.to_csv(output_name)
+
+    list_file(matrix,output_name)
+    
+    os.remove('vcon_file.txt')
+    
+    return
+    
+#get_args()
